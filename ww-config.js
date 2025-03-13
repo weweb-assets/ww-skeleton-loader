86c71788cec731f238eb677944840ac34f9ca572
Léo Coletta <mail.leo.coletta@gmail.com>
2025-03-13 14:15:31 +0100

    first commit

---
+++ ww-config.js
@@ -0,0 +1,17 @@
+export default {
+    options: {
+        displayAllowedValues: ['flex', 'inline-flex'],
+    },
+    inherit: {
+        type: 'ww-layout',
+    },
+    editor: {
+        label: {
+            en: 'Breadcrumbs',
+        },
+        icon: 'map',
+    },
+    properties: {},
+    triggerEvents: [],
+    actions: [],
+};
